97f79ef9dab125de0974391370547fd6b2f2fb92
Gavin Smith <gavin@gavinsmith.ca>
2011-07-03 23:14:23 -0700

    dividing into project files.. somehow

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 $(function(){
 	var form_fields = $('.form-field');
-	$(form_fields).addClass('active-label'); /*some people like JS-less degrading.*/
-	
+	$(form_fields).addClass('active-label'); /*some people like JS-less degrading. personally, I think those guys can go somewhere else on the internet.*/
 	$('input[type=text]',form_fields).keyup( function(){
 		var input_label = $('label[for='+$(this).attr('name')+']');
 		if( $(this).val() == '' ){
